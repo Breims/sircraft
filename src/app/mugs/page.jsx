bb2ec778d4727647abfffdedfe6029dddbab3af3
Breim <breim.dev@gmail.com>
2023-08-12 21:46:27 -0500

    [ADD] Recordatorio view

--- src/app/mugs/page.jsx
+++ src/app/mugs/page.jsx
@@ -1,10 +1,113 @@
+import Image from "next/image";
+import coverMugs from "@public/assets/images/lines/cover-mugs.jpg";
+import mugs1 from "@public/assets/images/lines/mugs1.jpg";
+import mugs2 from "@public/assets/images/lines/mugs2.jpg";
 import NavbarLines from "@/components/ui/NavbarLines";
+import * as constants from "../../style/linesStyles.js";
 
 const Mugs = () => {
   return (
     <>
-      <NavbarLines />
-      <p className="text-8xl">Hola Dos</p>
+      <header>
+        <NavbarLines />
+        <section className="relative">
+          <figure className="grid place-content-center">
+            <Image
+              className="lg:w-screen object-cover"
+              src={coverMugs}
+              alt="coverMugs"
+            />
+          </figure>
+          <div className="absolute h-full w-full top-0 bg-black opacity-70">
+            <p className="h-full mt-6 grid place-content-center place-items-center text-white font-HelveticaBold text-2xl md:text-5xl lg:text-8xl md:mt-0 animate-fade-down animate-once animate-duration-500 animate-delay-300">
+              MUGS PERSONALIZADOS
+            </p>
+          </div>
+        </section>
+      </header>
+
+      <main>
+        <section className="bg-slate-100">
+          <div className={constants.objectsGroupStart}>
+            <div className={constants.objectsDecaration}></div>
+            <div className={constants.objectsDecaration}></div>
+            <div className={constants.objectsDecaration}></div>
+          </div>
+          <div className={constants.contentPageGlobal}>
+            <div className={constants.contentText}>
+              <p className={constants.paragraph}>
+                En SirCraft nuestro producto estrella son las tazas de
+                sublimación, un artículo cotidiano que se transforma en una obra
+                de arte personalizada. Utilizamos un innovador proceso de
+                sublimación para imprimir tus diseños, fotos o mensajes
+                especiales en la superficie de la taza. La sublimación garantiza
+                una calidad de imagen nítida, duradera y resistente al paso del
+                tiempo, permitiéndote disfrutar de tus recuerdos favoritos o
+                mensajes inspiradores cada vez que tomes tu bebida favorita.
+              </p>
+              <p className={constants.paragraph}>
+                Nuestro proceso de sublimación es un arte en sí mismo.
+                Comenzamos con una taza de alta calidad y cubrimos su superficie
+                con una capa especial que actúa como lienzo para tus creaciones.
+                Luego, utilizando equipos de impresión avanzados y tintas de
+                primera categoría, transferimos cuidadosamente tus diseños al
+                material de la taza. La impresión se funde perfectamente con la
+                taza a nivel molecular, lo que garantiza una apariencia
+                brillante, sin bordes visibles y resistente a rayones o
+                desgaste.
+              </p>
+              <p>
+                Nuestro equipo de diseñadores y expertos en sublimación están
+                dedicados a asegurar que cada taza que creemos refleje la
+                belleza y calidad que esperas. Ya sea que elijas una taza con
+                una imagen personalizada, un mensaje conmovedor o un diseño
+                divertido, te garantizamos que el resultado será una pieza única
+                que te encantará compartir o regalar a tus seres queridos.
+              </p>
+            </div>
+
+            <div className={constants.containerImage}>
+              <Image className={constants.imagesFill} src={mugs1} alt="mugs1" />
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className={constants.objectsGroupEnd}>
+            <div className={constants.objectsDecaration}></div>
+            <div className={constants.objectsDecaration}></div>
+            <div className={constants.objectsDecaration}></div>
+          </div>
+
+          <div className={constants.contentPageGlobal}>
+            <div className={constants.containerImageOrderTwo}>
+              <Image className={constants.imagesFill} src={mugs2} alt="mugs2" />
+            </div>
+            <div className={constants.contentTextOrderTwo}>
+              <p className={constants.paragraph}>
+                En nuestra tienda, entendemos que cada taza de sublimación tiene
+                un significado especial y único para nuestros clientes. Nuestro
+                objetivo es brindarte una experiencia excepcional al permitirte
+                crear tazas que cuenten tu historia, celebren tus momentos más
+                especiales o expresen tu personalidad. Imagina disfrutar de tu
+                café matutino en una taza que contiene los momentos más
+                preciados de tu vida o regalar una taza personalizada como un
+                gesto significativo para alguien especial.
+              </p>
+              <p className={constants.paragraph}>
+                Además de ofrecerte tazas personalizadas, también queremos
+                brindarte una experiencia de compra sin complicaciones y un
+                servicio al cliente excepcional. Nos enorgullecemos de la
+                calidad de nuestros productos y del placer que brindan a
+                nuestros clientes. Esperamos que nuestra pasión por la
+                sublimación se refleje en cada taza que entregamos, y que cada
+                taza se convierta en una pieza única y valiosa en tu vida
+                diaria.
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
---
+++ src/app/recordatorios/page.jsx
@@ -0,0 +1,129 @@
+import Image from "next/image";
+import coverRecordatorio from "@public/assets/images/lines/cover-recordatorio.jpg";
+import recordatorio1 from "@public/assets/images/lines/recordatorio1.jpg";
+import recordatorio2 from "@public/assets/images/lines/recordatorio2.jpg";
+import NavbarLines from "@/components/ui/NavbarLines";
+import * as constants from "../../style/linesStyles.js";
+
+const Recordatorios = () => {
+  return (
+    <>
+      <header>
+        <NavbarLines />
+        <section className="relative">
+          <figure className="grid place-content-center">
+            <Image
+              className="lg:w-screen object-cover"
+              src={coverRecordatorio}
+              alt="coverRecordatorio"
+            />
+          </figure>
+          <div className="absolute h-full w-full top-0 bg-black opacity-70">
+            <p className="h-full mt-6 grid place-content-center place-items-center text-white font-HelveticaBold text-2xl md:text-5xl lg:text-7xl md:mt-0 animate-fade-down animate-once animate-duration-500 animate-delay-300">
+              ¡RECORDATORIOS DE TODO TIPO!
+            </p>
+          </div>
+        </section>
+      </header>
+
+      <main>
+        <section>
+          <div className={constants.objectsGroupStart}>
+            <div className={constants.objectsDecaration}></div>
+            <div className={constants.objectsDecaration}></div>
+            <div className={constants.objectsDecaration}></div>
+          </div>
+          <div className={constants.contentPageGlobal}>
+            <div className={constants.contentText}>
+              <p className={constants.paragraph}>
+                Nuestros recordatorios son pequeñas piezas artísticas y
+                confeccionadas a mano que capturan momentos inolvidables de tu
+                vida. Desde delicados detalles en porcelanicrón hasta adorables
+                creaciones en crochet y peluches, cada uno de nuestros
+                recordatorios está diseñado para evocar emociones y transmitir
+                mensajes especiales. Además, no olvides que también ofrecemos
+                nuestros famosos mugs personalizados, que son perfectos para
+                conmemorar cualquier ocasión especial.
+              </p>
+              <p className={constants.paragraph}>
+                En nuestra tienda, la pasión por la artesanía y la atención al
+                detalle son nuestro sello distintivo. Cada recordatorio se crea
+                con esmero y dedicación por nuestro talentoso equipo de
+                artesanos y diseñadores. Cuando eliges un recordatorio en
+                porcelanicrón, nuestro equipo experto da vida a la porcelana
+                fría modelándola con habilidad para reflejar tus momentos más
+                preciados. Si prefieres un recordatorio en crochet, nuestros
+                hábiles tejedores crean intrincados diseños utilizando hilos de
+                alta calidad, dándole un toque tierno y delicado. Asimismo,
+                nuestros peluches son confeccionados con materiales suaves y
+                seguros para brindar la máxima ternura en cada detalle.
+              </p>
+              <p>
+                En el caso de nuestros mugs personalizados, aplicamos el mismo
+                proceso de sublimación que utilizamos en nuestra línea de tazas
+                para café y bebidas. Cada mug se imprime cuidadosamente con tus
+                diseños, fotografías o mensajes, asegurando que el resultado sea
+                una obra de arte funcional que te acompañará en cada sorbo de tu
+                bebida favorita.
+              </p>
+            </div>
+
+            <div className={constants.containerImage}>
+              <Image
+                className={constants.imagesFill}
+                src={recordatorio1}
+                alt="recordatorio1"
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className="bg-slate-100">
+          <div className={constants.objectsGroupEnd}>
+            <div className={constants.objectsDecaration}></div>
+            <div className={constants.objectsDecaration}></div>
+            <div className={constants.objectsDecaration}></div>
+          </div>
+
+          <div className={constants.contentPageGlobal}>
+            <div className={constants.containerImageOrderTwo}>
+              <Image
+                className={constants.imagesFill}
+                src={recordatorio2}
+                alt="recordatorio2"
+              />
+            </div>
+            <div className={constants.contentTextOrderTwo}>
+              <p className={constants.paragraph}>
+                En nuestra tienda, creemos que los recordatorios son más que
+                simples objetos; son expresiones de amor, gratitud y cariño.
+                Cada recordatorio que creamos tiene un propósito significativo:
+                celebrar los momentos más especiales de la vida, honrar a seres
+                queridos, transmitir mensajes de aliento y aprecio, y preservar
+                los recuerdos más queridos.
+              </p>
+              <p className={constants.paragraph}>
+                Nuestro objetivo es proporcionarte recordatorios que toquen el
+                corazón y brinden alegría a quienes los reciben. Ya sea que
+                elijas un delicado adorno en porcelanicrón, un tierno peluche,
+                un encantador detalle en crochet o un mug personalizado, nuestro
+                deseo es que cada pieza sea un testimonio duradero de tus
+                momentos más felices.
+              </p>
+              <p className={constants.paragraph}>
+                Desde bodas, cumpleaños, baby showers, hasta ocasiones
+                especiales o simplemente para mostrar a alguien que te importa,
+                nuestros recordatorios están diseñados para crear recuerdos
+                inolvidables. No importa qué tipo de recordatorio elijas,
+                sabemos que cada uno llevará consigo un mensaje y un lugar
+                especiales en tu corazón.
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Recordatorios;
