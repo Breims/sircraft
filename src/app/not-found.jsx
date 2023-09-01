700bada00ef4cc14f72eee8cf38e9a36bd689d5b
Breim <breim.dev@gmail.com>
2023-09-01 00:54:52 -0500

    [UPDATE]

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -10,7 +10,7 @@ const NotFound = () => {
           Uh-oh!
         </p>
 
-        <p className="mt-4 text-gray-500">We can't find that page.</p>
+        <p className="mt-4 text-gray-500">Pägina en Construcción 😊</p>
 
         <Link
           href="/"
